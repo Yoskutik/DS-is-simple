51915de27f006b1f57057907e3831b38b9e19f4c
Yoskutik <yosktuik@gmail.com>
2020-11-10 01:04:52 +0300

    Больше стилей для Switch

--- components/switch/switchViewModel.ts
+++ components/switch/switchViewModel.ts
@@ -1,11 +1,38 @@
+import { observable } from 'mobx';
 import React from 'react';
 
 export default class SwitchViewModel {
+    @observable
+    public toggled: boolean = null;
+
     public onItemClick = (evt: React.MouseEvent): void => {
-        const item = (evt.target as HTMLWebViewElement).closest('.page__switch_item') as HTMLDivElement;
+        const item = (evt.target as HTMLWebViewElement).closest<HTMLDivElement>('.page__switch_item');
+        const switchElement = item.closest('.page__switch');
+
+        if (window.innerWidth < 550) {
+            this.toggled = !this.toggled;
+            const items = Array.from(switchElement.children);
+            if (!this.toggled) {
+                const index = items.indexOf(item);
+                items.forEach((it: HTMLDivElement) => {
+                    const { style } = it;
+                    style.top = `calc(-${index}rem - ${index * 9}px)`;
+                });
+
+                const lastActive = switchElement.querySelector('.page__switch_item.active');
+                lastActive && lastActive.classList.remove('active');
+                item.classList.add('active');
+            } else {
+                items.forEach((it: HTMLDivElement) => {
+                    const { style } = it;
+                    style.top = null;
+                });
+            }
+            return;
+        }
+
         if (!item || item.classList.contains('active')) return;
 
-        const switchElement = item.closest('.page__switch');
         switchElement.querySelector('.page__switch_item.active').classList.remove('active');
         item.classList.add('active');
     };
--- views/pages/NLP/RussianHomework/index.tsx
+++ views/pages/NLP/RussianHomework/index.tsx
@@ -2,16 +2,17 @@ import { observer } from 'mobx-react-lite';
 import React, { useState } from 'react';
 import { Page, Title, Switch, SwitchItem } from '@components';
 
+const Preview = React.lazy(() => import('./preview'));
 const Data = React.lazy(() => import('./data'));
 
 const NlpRussianHomework: React.FC = observer(() => {
-    const [Content, setContent] = useState<React.FC>(Data);
+    const [Content, setContent] = useState<React.FC>(Preview);
 
     return (
         <Page>
             <Title>Домашка по русскому</Title>
             <Switch>
-                <SwitchItem onClick={() => setContent(Data)} value="Превью" active />
+                <SwitchItem onClick={() => setContent(Preview)} value="Превью" active />
                 <SwitchItem onClick={() => setContent(Data)} value="Код" />
                 <SwitchItem onClick={() => setContent(React.Fragment)} value="Данные" />
             </Switch>
